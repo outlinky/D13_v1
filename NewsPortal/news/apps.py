d49894fe9282b66ffdc464254f6bbafd75b9a0b3
outlinky <j4red@ya.ru>
2022-04-28 23:51:36 +0700

    v2

--- NewsPortal/news/apps.py
+++ NewsPortal/news/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-
+import redis
 
 class NewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -7,3 +7,10 @@ class NewsConfig(AppConfig):
 
     def ready(self):
         import news.signals
+
+
+red = redis.Redis(
+    host='redis-12373.c282.east-us-mz.azure.cloud.redislabs.com',
+    port=12373,
+    password='Dm5vcIB79mdAE9wZoIi9osIz2zvsiZix'
+)
